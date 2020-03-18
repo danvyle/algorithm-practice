a78eaf8ccb03cb2f2c5baf1cb701beb6a4fa1ba3
Dan-Vy Le <danvyle85@gmail.com>
2020-03-17 17:12:49 -1000

    unsolved car-passing

--- cars-driving.js
+++ cars-driving.js
@@ -1,19 +1,21 @@
 //----------problem
 
-Given an array of 1s and 0s, 0 representing a car driving east, 1 representing a car driving west. 
+// Given an array of 1s and 0s, 0 representing a car driving east, 1 representing a car driving west. 
 
 
-input: [0, 1, 1, 1, 0, 0]
-output: 3
+// input: [0, 1, 1, 1, 0, 0]
+// output: 3
 
-input: [0,0,0,1,1,1]
-output: 9
+// input: [0,0,0,1,1,1]
+// output: 9
 
 
 
 //----------pseudocode
 
 
+// I want to count every time there is a 0 and it passes a 1.
+// I'll need to set a counter for every time this happens and return the result at the end.
 
 
 
@@ -22,10 +24,22 @@ output: 9
 
 //----------solution
 
+let ar = [0, 1, 1, 1, 0, 0]
+function carsPassing(ar) {
+    let counter = 0
+    for (let i = ar.length; i <= 0; i--) {
+        if (ar[i] === 1) {
+            if(ar[i-1] === 0) {
+                counter++
+            }
+        }
+    }
+    return counter
+}
 
 
 
-
+console.log(carsPassing(ar))
 
 
 
