b368029fff6d7b1ae41b364f716db6b3cad1ad6f
Dan-Vy Le <danvyle85@gmail.com>
2020-09-03 13:52:14 -0700

    rotate left

---
+++ rotate-left.js
@@ -0,0 +1,38 @@
+// ------problem--------
+// A left rotation operation on an array shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become .
+
+// Given an array  of  integers and a number, , perform  left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
+
+// Function Description
+
+// Complete the function rotLeft in the editor below. It should return the resulting array of integers.
+
+// rotLeft has the following parameter(s):
+
+// An array of integers .
+// An integer , the number of rotations.
+// Input Format
+
+// The first line contains two space-separated integers  and , the size of  and the number of left rotations you must perform.
+// The second line contains  space-separated integers .
+
+// Sample Input
+
+// 5 4
+// 1 2 3 4 5
+// Sample Output
+
+// 5 1 2 3 4
+
+// ------solution---------
+
+
+function rotLeft(a, d) {
+    let result = a.slice(d).concat(a.slice(0,d));
+    console.log(result);
+}
+
+rotLeft([1,2,3,4,5], 4);
+
+// -----space time-----
+// O(N)
